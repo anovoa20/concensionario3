65ac2e0f6f1341920c551f8f882dd0c14ae0b798
Andres Novoa <anovoa20@gmail.com>
2022-08-27 10:31:21 -0500

    ajustes finales clase 13

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="bg-red-400">
       <h1>Navbar</h1>
+      <ul className="flex w-full justify-between my-3">
+        <li>logo</li>
+        <li>Navegacion1</li>
+        <li>Navegacion1</li>
+        <li>Navegacion1</li>
+        <li className="px-3">
+          <Link to="/login">
+            <button className="bg-indigo-500 p-2 text-white rounded-lg shadow-sm hover:bg-indigo-700">
+              Iniciar Sesión
+            </button>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <h2>página para el login</h2>
+    <div className="flex flex-col w-full justify-center items-center">
+      <h2 className="m-3 text-center text-3xl font-extrabold text-gray-900">
+        Inicia sesión en tu cuenta
+      </h2>
+      <form className="mt-8 max-w-md">
+        <div>
+          <input
+            className="appearance-none focus:outline-none px-3 py-2 border border-gray-300 text-gray-900 rounded-md focus:ring-1 focus:ring-indigo-500"
+            type="email"
+            placeholder="pepe@account.com"
+            required
+          />
+          <input
+            className="appearance-none focus:outline-none px-3 py-2 border border-gray-300 text-gray-900 rounded-md focus:ring-1 focus:ring-indigo-500 "
+            type="password"
+            required
+          />
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <label htmlFor="recuerdame">
+              <input type="checkbox" name="recuerdame" />
+              Recuérdame
+            </label>
+          </div>
+          <div>
+            <Link to="/">Olvidaste tu contraseña</Link>
+          </div>
+          <div>
+            <Link to="/admin">
+              <button type="submit">Iniciar Sesión</button>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <button>continúa con Google</button>
+        </div>
+      </form>
     </div>
   );
 };
